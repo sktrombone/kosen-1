145bf22458078b2083c055dbed39e84de127120f
sakakendo0321 <sakakendo0321@gmail.com>
2017-04-28 10:20:59 +0900

    Add files via upload

---
+++ kp/kadai2-3.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MEM (1024*32)
+
+void expand(char data[512],char pattern[64][64]){
+	for(int i=0;i<512;i++){
+		for(int j=7;j>=0;j--){
+			if(data[i]&(1<<j)){
+				pattern[i/8][(i%8+1)*8-j]=0;
+			}else{
+				pattern[i/8][(i%8+1)*8-j]=1;
+			}
+		}
+	}
+}
+
+void printimg(unsigned char d[]){
+	for(int i=0;i<512;i++){
+		for(int j=7;j>=0;j--){
+			if(d[i]&(1<<j)){
+				printf("*");
+			}else{
+				printf(".");
+			}
+		}
+		if(i%8==7 ){
+			printf("\n");
+		}
+	}
+}
+
+void compress(char data[512],char pattern[64][64]){
+	for(int i=0;i<64;i++){
+		for(int j=0;j<64;j++){
+//			pattern[]data[i][j];
+			data[64/8][j]|=pattern[i/8][8-i%8];
+			
+		}
+	}
+}
+
+int main(int argc,char **argv){
+	unsigned char d[MEM];
+	char p[64][64];
+	FILE *fp;
+	char fname[128];
+	for(int file=1;file<47;file++){
+		for(int i=0;i<MEM;i++)	d[i]=0;
+		snprintf(fname,sizeof(fname),"%02d.img",file);
+		printf("input file name %s\n",fname);
+		fp=fopen(fname,"rb");
+		if(fp==NULL){
+			printf("can't open %s",fname);
+			exit(1);
+		}
+		for(int i=0;i<20;i++){
+			fread(d,512,1,fp);
+			printimg(d);
+			getchar();
+		}
+		fclose(fp);
+	}
+	return 0;
+
+}
